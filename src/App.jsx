ae4e5e3dc053ce2f70cccae291065067b026680a
Thejas <teju270934@gmail.com>
2024-10-25 14:52:28 +0530

    updated the home compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { SmoothScrollHero } from "./components/Home";
+
 function App() {
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get("http://10.60.201.123/greeting");
+      console.log(response.data);
+    }
+    getData();
+  }, []);
   return (
     <div className="bg-red-500">
-      hello
+      <SmoothScrollHero />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/greeting": {
+        target: "http://10.60.201.123",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
